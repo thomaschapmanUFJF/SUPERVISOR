606831950d9425b0f152196b5116d4c1b8433524
Thomas Chapman <thoadchapman@gmail.com>
2026-05-31 13:19:05 -0300

    feat: adicionar cabeçalho à inicialização da classe Sheet

--- Sheet.py
+++ Sheet.py
@@ -5,10 +5,13 @@ HEADER = CONFIG['CSV']['HEADER']
 class Sheet():
     def __init__(self):
         self.sheet = []
-        self.sheet.append(HEADER)
     def __iter__(self):
         return iter(self.sheet)
     def __getitem__(self, index):
         return self.sheet[index]
     def add_row(self, row):
         self.sheet.append(row)
+    def add_row_dict(self, row_dict):
+        self.sheet.append(row_dict)
+    def __print__(self):
+        return f"Sheet(sheet={self.sheet})"
