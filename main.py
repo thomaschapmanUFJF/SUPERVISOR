cd7e995055fc80ba9cd3cb62dc60edfce8e55736
Thomas Chapman <thoadchapman@gmail.com>
2026-05-31 21:48:17 -0300

    feat: remover função main e iniciar servidor WebSocket em thread

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
+import uvicorn
+
 from Tradutor import Tradutor
 from Escrivao import Escrivao
+from app.backend.app import app
+from motor import motor
+import threading
 
 tradutor = Tradutor()
 escrivao = Escrivao()
 
-def main():
-    print('INICIANDO...')
-    tentativas = 0
-    rows_recebidas = 0
-    while True:
-        try:
-            row = tradutor.decode_row()
-            escrivao.add_row(row)
-            rows_recebidas += 1
-            if rows_recebidas % 10 == 0:
-                print(f"ROWS RECEBIDAS: {rows_recebidas}")
-        except TimeoutError:
-            print('ESPERANDO DADOS DO FOGUETE...')
-            tentativas += 1
-            if tentativas >= 5:
-                print('NENHUM DADO RECEBIDO APÓS 5 TENTATIVAS. ENCERRANDO...')
-                break
-        except KeyboardInterrupt:
-            print('TECLADO APERTADO. ENCERRANDO...')
-            break
-        except Exception as e:
-            print(f'ERRO INESPERADO: {e}')
-            break
-
 if __name__ == '__main__':
-    main()
-    escrivao.save_csv()
+    thread_motor = threading.Thread(target=motor, daemon=True)
+    thread_motor.start()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
