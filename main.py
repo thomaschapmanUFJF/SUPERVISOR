35cf9fdb6daf278d515dd7645235fa01b85cf0e0
Thomas Chapman <thomas.chapman@estudante.ufjf.br>
2026-06-07 20:12:46 -0300

    chore(config): remover config.py e imports obsoletos

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import uvicorn
-
-from Tradutor import Tradutor
-from Escrivao import Escrivao
 from app.backend.app import app
 from motor import motor
 import threading
-
-tradutor = Tradutor()
-escrivao = Escrivao()
 
 if __name__ == '__main__':
     thread_motor = threading.Thread(target=motor, daemon=True)
