12af7e8ba6c4e196484db23daaab17e975c0fff1
Thomas Chapman <thomas.chapman@estudante.ufjf.br>
2026-06-12 15:08:35 -0300

    feat(main): adicionar argumento de teste para modo simulação

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import uvicorn
 from app.backend.app import app
 from motor import motor
 import threading
+import sys
 
-if __name__ == '__main__':
-    thread_motor = threading.Thread(target=motor, daemon=True)
-    thread_motor.start()
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+test = True if len(sys.argv) > 1 and sys.argv[1] == '1' else False
+thread_motor = threading.Thread(target=motor, args=(test,), daemon=True)
+thread_motor.start()
+uvicorn.run(app, host="0.0.0.0", port=8000)
