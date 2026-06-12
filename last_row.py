287a5a24c068084e187f389573252958698252e9
Thomas Chapman <thomas.chapman@estudante.ufjf.br>
2026-06-12 15:08:35 -0300

    refactor(last_row): adicionar flag de estado de recebimento de dados

--- last_row.py
+++ last_row.py
@@ -1,10 +1,13 @@
 class last_row():
     def __init__(self):
+        self.recebendo_dados = True
         self.last_row = None
     def update_row(self, row):
         self.last_row = row
     def get_row(self):
         return self.last_row
+    def set_recebendo_dados(self, booleano):
+        self.recebendo_dados = booleano
 
 global last_row_instance 
 last_row_instance = last_row()
