e94c48883de83206e8419f017224fc9c53be4782
Thomas Chapman <thomas.chapman@estudante.ufjf.br>
2026-06-13 18:17:12 -0300

    fix(foguete): corrige ordem dos componentes do quaternion

---
+++ frontend/src/FogueteModelo.jsx
@@ -0,0 +1,35 @@
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { useTelemetria } from './Telemetria';
+import { useGLTF, Center } from '@react-three/drei'; 
+
+export default function FogueteModelo(){
+  const foguete3D = useRef();
+  const { scene } = useGLTF('/rocket.glb');
+
+  useFrame(() => {
+    if (!foguete3D.current) return
+    const pacote = useTelemetria.getState().atual
+    if (!pacote) return
+    
+    if (pacote.q1 === 0 && pacote.q2 === 0 && pacote.q3 === 0 && pacote.q4 === 0) return
+
+    foguete3D.current.quaternion.set(
+      pacote.q2, // x
+      pacote.q3, // y
+      pacote.q4, // z
+      pacote.q1  // w
+    )
+  })
+
+  return (
+    <group ref={foguete3D}>
+      <Center> 
+        <primitive 
+          object={scene} 
+          scale={1} 
+        />
+      </Center>
+    </group>
+  );
+}
