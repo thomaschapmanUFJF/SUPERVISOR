990fffaeeefcebfe3903b7b1c9f94097d9e90b5b
Thomas Chapman <thomas.chapman@estudante.ufjf.br>
2026-06-14 20:30:32 -0300

    feat(telemetria): implementar limite de histórico de dados

--- frontend/src/Telemetria.jsx
+++ frontend/src/Telemetria.jsx
@@ -1,16 +1,29 @@
 import { create } from 'zustand'
 
+const MAX_PONTOS = 60
+const THROTTLE_PACOTES = 10
 export const useTelemetria = create((set) => ({
     atual: null,
     posicaoAtual: null,
     historicoAltitude: [],
     historicoPosicao: [],
-  adicionar: (telemetria) => set((state) => (
+    contador: 0,
+  adicionar: (telemetria) => set((state) => {
+    const proximoContador = state.contador+1
+    const atualiza = proximoContador % THROTTLE_PACOTES === 0
+    return (
     { 
       atual: telemetria, 
+      contador: proximoContador,
       posicaoAtual: { latitude: telemetria.latitude, longitude: telemetria.longitude },
-      historicoAltitude: [...state.historicoAltitude.slice(-99), { time: telemetria.time, altitude: telemetria.altitude }],
-      historicoPosicao: [...state.historicoPosicao, { latitude: telemetria.latitude, longitude: telemetria.longitude }],
+      historicoAltitude: atualiza ? 
+      [...state.historicoAltitude, { time: telemetria.time, altitude: telemetria.altitude }].splice(-MAX_PONTOS)
+      : state.historicoAltitude,
+      historicoPosicao: atualiza ? 
+      [...state.historicoPosicao, { latitude: telemetria.latitude, longitude: telemetria.longitude }].splice(-MAX_PONTOS)
+      : state.historicoPosicao
     }
-  )),
+  )}
+),
+
 }))
