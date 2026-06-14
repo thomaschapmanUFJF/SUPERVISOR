dd59ac6c10b30846d173cd5dae57b51f257073f8
Thomas Chapman <thomas.chapman@estudante.ufjf.br>
2026-06-14 20:30:32 -0300

    chore(start): reorganizar comandos de inicialização

--- start.bat
+++ start.bat
@@ -5,18 +5,16 @@ echo ==============================================
 echo [1] Iniciando o Servidor de Desenvolvimento do React (Vite)...
 :: Entra na pasta do frontend para rodar o comando do npm
 start "FRONTEND - React" cmd /c "cd frontend && npm run dev"
-
 timeout /t 3 /nobreak > NUL
-echo [2] Iniciando o Backend e Motor de Leitura (main.py)...
-:: O main.py e o "cerebro" que liga o motor e o servidor web ao mesmo tempo
-start "ESTACAO SOLO - Backend" cmd /k "cd backend && python main.py 0"
-
-timeout /t 5 /nobreak > NUL
-
-echo [3] Abrindo o Painel no Navegador...
+echo [2] Abrindo o Painel no Navegador...
 :: O Vite por padrao usa a porta 5173. 
 :: Se o seu abrir em outra (ex: 3000), altere o numero abaixo.
 start http://localhost:5173
+
+timeout /t 3 /nobreak > NUL
+echo [3] Iniciando o Backend e Motor de Leitura (main.py)...
+:: O main.py e o "cerebro" que liga o motor e o servidor web ao mesmo tempo
+start "ESTACAO SOLO - Backend" cmd /k "cd backend && python main.py 0"
 
 timeout /t 2 /nobreak > NUL
 
