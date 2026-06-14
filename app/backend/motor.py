f4f88a49621a9552be13a54ce324a2f2c09f3d97
Thomas Chapman <thomas.chapman@estudante.ufjf.br>
2026-06-14 19:07:20 -0300

    feat(motor): implementar função motor para processamento de dados

---
+++ app/backend/motor.py
@@ -0,0 +1,55 @@
+import csv
+from dataclasses import asdict
+import struct
+
+from app.backend.mensageiro import Mensageiro
+from app.backend.last_row import last_row_instance
+from config.config import CONFIG
+from app.backend.schema import HEADER
+from app.backend.schema import FORMAT
+from app.backend.Row import Row
+
+FILENAME = CONFIG['CSV']['FILENAME']
+
+def write_header():
+    with open(FILENAME,'w',newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=HEADER)
+        writer.writeheader()
+def update_csv(row):
+    with open(FILENAME, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=HEADER)
+        writer.writerow(asdict(row))
+def decode_row(data):
+    decoded = struct.unpack(FORMAT, data)        
+    return Row(*decoded)
+
+def motor(test):
+    print('INICIANDO...')
+    mensageiro = Mensageiro(test)
+    tentativas = 0
+    rows_recebidas = 0
+    write_header()
+    while True:
+        try:
+            data = mensageiro.get_row()
+            row = decode_row(data)
+            last_row_instance.update_row(row)
+            update_csv(row)
+            rows_recebidas += 1
+
+            if rows_recebidas % 10 == 0:
+                print(f"ROWS RECEBIDAS: {rows_recebidas}")
+
+        except TimeoutError:
+            print('ESPERANDO DADOS DO FOGUETE...')
+            tentativas += 1
+            if tentativas >= 10:
+                print('NENHUM DADO RECEBIDO APÓS 10 TENTATIVAS. ENCERRANDO...')
+                last_row_instance.update_row(None)
+                break
+        except KeyboardInterrupt:
+            print('TECLADO APERTADO. ENCERRANDO...')
+            break
+        except Exception as e:
+            print(f'ERRO INESPERADO: {e}')
+            break
