d8ce423eaabbb3bb97843c487943e3dd301ee328
Thomas Chapman <thomas.chapman@estudante.ufjf.br>
2026-06-15 13:07:25 -0300

    Atualiza a versão do pacote @react-three/drei para 9.88.0 e melhora o script de setup para instalação de dependências

--- setup.bat
+++ setup.bat
@@ -2,21 +2,26 @@
 echo ==============================================
 echo               INICIANDO SETUP
 echo ==============================================
-echo [1] Instalando dependências do React (Vite)...
-cmd "cd frontend && npm install"
+echo [1] Instalando dependencias do React...
+pushd frontend
+npm install
 if %errorlevel% neq 0 (
     echo ERRO: instalacao do npm falhou!
+    popd
     pause
     exit /b 1
 )
+popd
+
 echo.
-echo [2] Instalando FastAPI
-cmd "pip install fastapi uvicorn pyserial"
+echo [2] Instalando dependencias do Python...
+pip install fastapi uvicorn pyserial
 if %errorlevel% neq 0 (
-    echo ERRO: instalacao do fastapi falhou!
+    echo ERRO: instalacao do pip falhou!
     pause
     exit /b 1
 )
+
 echo ==============================================
 echo               SETUP COMPLETO!
 echo ==============================================
