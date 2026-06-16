9df9d1572ad2ce20d28b2160b5fcaab8d4d2a3e0
Thomas Chapman <thomas.chapman@estudante.ufjf.br>
2026-06-15 23:42:26 -0300

    refactor(setup): reorganizar instalação de dependências do Python e React no script de setup

--- setup.bat
+++ setup.bat
@@ -5,7 +5,37 @@ echo ==============================================
 echo               INICIANDO SETUP
 echo ==============================================
 
-echo [1] Instalando dependencias do React...
+echo [1] Instalando dependencias do Python...
+
+REM Check if pip exists
+where pip >nul 2>&1
+if %errorlevel% neq 0 (
+    echo ERRO: pip nao encontrado no PATH!
+    echo Certifique-se que o Python esta instalado e adicionado ao PATH
+    pause
+    exit /b 1
+)
+
+REM Install Python packages
+pip install fastapi[standard]
+if %errorlevel% neq 0 (
+    echo ERRO: instalacao do pip falhou!
+    pause
+    exit /b 1
+)
+
+REM Install Pyserial
+pip install pyserial
+
+if %errorlevel% neq 0 (
+    echo ERRO: instalacao do pyserial
+    pause
+    exit /b 1
+)
+
+echo.
+
+echo [2] Instalando dependencias do React...
 
 REM Check if frontend folder exists
 if not exist "frontend" (
@@ -26,26 +56,6 @@ popd
 REM Check if npm install failed
 if !NPM_ERROR! neq 0 (
     echo ERRO: instalacao do npm falhou com codigo !NPM_ERROR!
-    pause
-    exit /b 1
-)
-
-echo.
-echo [2] Instalando dependencias do Python...
-
-REM Check if pip exists
-where pip >nul 2>&1
-if %errorlevel% neq 0 (
-    echo ERRO: pip nao encontrado no PATH!
-    echo Certifique-se que o Python esta instalado e adicionado ao PATH
-    pause
-    exit /b 1
-)
-
-REM Install Python packages
-pip install fastapi uvicorn pyserial
-if %errorlevel% neq 0 (
-    echo ERRO: instalacao do pip falhou!
     pause
     exit /b 1
 )
