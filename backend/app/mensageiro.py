55c3eb786fd57827187d7488b65d04edd184f038
Thomas Chapman <thomas.chapman@estudante.ufjf.br>
2026-06-15 23:24:14 -0300

    refactor(mensageiro): mover inicialização da porta serial para dentro do bloco condicional de teste

--- backend/app/mensageiro.py
+++ backend/app/mensageiro.py
@@ -4,21 +4,22 @@ from testes.testes import through_csv
 from config.config import CONFIG
 from app.schema import FORMAT
 
-BAUDRATE = CONFIG['SERIAL']['BAUDRATE']
-READ_PORT_PATH = CONFIG['SERIAL']['READ_PORT']['PATH']
-
 class Mensageiro:
     def __init__(self, test):
         self.test = test
-        self.port = serial.Serial(port=READ_PORT_PATH, baudrate=BAUDRATE, timeout=1)
         self.format = FORMAT
         self.size = struct.calcsize(self.format)
         self.through_csv = through_csv()
+        if (test):
+            BAUDRATE = CONFIG['SERIAL']['BAUDRATE']
+            READ_PORT_PATH = CONFIG['SERIAL']['READ_PORT']['PATH']
+            self.port = serial.Serial(port=READ_PORT_PATH, baudrate=BAUDRATE, timeout=1)
 
     def through_virtual_port(self):
-        data = self.port.read(self.size)
-        if len(data) != self.size:
-            raise TimeoutError(f"TIMEOUT NA PORTA. Esperava {self.size} bytes, recebeu {len(data)}")
+        if (self.test):
+            data = self.port.read(self.size)
+            if len(data) != self.size:
+                raise TimeoutError(f"TIMEOUT NA PORTA. Esperava {self.size} bytes, recebeu {len(data)}")
         return data
     
     def get_row(self):
