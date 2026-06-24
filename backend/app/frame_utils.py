1095bc9fae2cc7cee4542d6f2d1d7825aa9c997b
Thomas Chapman <thomas.chapman@estudante.ufjf.br>
2026-06-24 20:49:40 -0300

    refactor(frame_utils): ajustar referências de configuração

--- backend/app/frame_utils.py
+++ backend/app/frame_utils.py
@@ -1,53 +1,48 @@
-from crc import Calculator, Crc16
+from crc import Calculator
 import struct
 import random
-from app.schema import PAYLOAD_FORMAT, FRAME_SCHEMA, HEADER_SIZE
+from app.schema import PAYLOAD_FORMAT, FRAME_SCHEMA
 from config.test import TEST_CONFIG
 from config.config import CONFIG
 
-CRC_TYPE = CONFIG['CRC']['TYPE']
+CRC = CONFIG['crc']
 
-PROBS = TEST_CONFIG['PROBS']
-PROB_SWAP = PROBS['SWAP']
-PROB_ADD_RAND = PROBS['ADD_RAND']
-PROB_CUT_IN_HALF = PROBS['CUT_IN_HALF']
-PROB_DROP = PROBS['DROP']
+PROBS = TEST_CONFIG['probs']
+SWAP = PROBS['swap']
+ADD_RAND = PROBS['add_rand']
+CUT_IN_HALF = PROBS['cut_in_half']
+DROP = PROBS['drop']
 
-SYNC_BYTE_1 = FRAME_SCHEMA['header']['sync1']['value']
-SYNC_BYTE_2 = FRAME_SCHEMA['header']['sync2']['value']
-CRC_SIZE = FRAME_SCHEMA['crc']['size']
-CRC_ENDIANNESS = FRAME_SCHEMA['crc']['endianness']
-TYPE_IDX = FRAME_SCHEMA['header']['type']['idx']
-LENGTH_IDX = FRAME_SCHEMA['header']['length']['idx']
-CRC_IDX = FRAME_SCHEMA['crc']['idx']
-
+HEADER = FRAME_SCHEMA['header']
+SYNC_BYTE_1 = HEADER['sync1']['value']
+SYNC_BYTE_2 = HEADER['sync2']['value']
 
 def calculate_crc(type, length, payload):
-    calculator = Calculator(CRC_TYPE)
+    calculator = Calculator(CRC['type'])
     data = bytes([type]) + bytes([length]) + payload
     checksum = calculator.checksum(data)
     return checksum
 
 def calculate_crc_from_frame(frame):
-    type_byte = frame[TYPE_IDX]
-    length_byte = frame[LENGTH_IDX]
-    payload = frame[HEADER_SIZE : HEADER_SIZE + length_byte]
+    type_byte = frame[HEADER['type']['idx']]
+    length_byte = frame[HEADER['length']['idx']]
+    payload = frame[HEADER['size'] : HEADER['size'] + length_byte]
     
-    calculator = Calculator(CRC_TYPE)
+    calculator = Calculator(CRC['type'])
     data = bytes([type_byte]) + bytes([length_byte]) + payload
     crc_value = calculator.checksum(data)
     return crc_value
 
 def crc_ok(frame):
     crc_value = calculate_crc_from_frame(frame)
-    expected_crc = crc_value.to_bytes(CRC_SIZE, CRC_ENDIANNESS)
-    package_crc = frame[CRC_IDX : CRC_IDX + CRC_SIZE]
+    expected_crc = crc_value.to_bytes(CRC['size'], CRC['endianness'])
+    package_crc = frame[CRC['idx'] : CRC['idx'] + CRC['size']]
     return expected_crc == package_crc
 
 def build_frame(payload, type=0x01):
     length = len(payload)
     crc_value = calculate_crc(type, length, payload)
-    crc_bytes = crc_value.to_bytes(CRC_SIZE, CRC_ENDIANNESS)
+    crc_bytes = crc_value.to_bytes(CRC['size'], CRC['endianness'])
     return bytearray([
         SYNC_BYTE_1,
         SYNC_BYTE_2,
@@ -59,21 +54,21 @@ def build_frame(payload, type=0x01):
     ])
 
 def add_noise(frame):
-    if random.random() <= PROB_DROP:
+    if random.random() <= DROP:
         print('DROP!')
         return None
     
     novo_frame = frame[:]
-    if random.random() <= PROB_SWAP:
+    if random.random() <= SWAP:
         idx = random.randrange(len(novo_frame))
         novo_frame[idx] = random.randint(0, 255)
         print('SWAP!')
     
-    if random.random() <= PROB_ADD_RAND:
+    if random.random() <= ADD_RAND:
         novo_frame.insert(0, random.randint(0, 255))
         print('RANDOM!')
     
-    if random.random() <= PROB_CUT_IN_HALF:
+    if random.random() <= CUT_IN_HALF:
         meio = len(novo_frame) // 2
         novo_frame = novo_frame[:meio]
         print('CUT IN HALF!')
@@ -81,8 +76,8 @@ def add_noise(frame):
     return novo_frame
 
 def get_payload(frame):
-    length_byte = frame[LENGTH_IDX]
-    return frame[HEADER_SIZE : HEADER_SIZE + length_byte]
+    length_byte = frame[HEADER['length']['idx']]
+    return frame[HEADER['size'] : HEADER['size'] + length_byte]
 
 if __name__ == "__main__":
     from testes.test import generate_packages
